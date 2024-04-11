2752be8a3aa68c08209f2682b3c158adf1d61e16
nmadden2027 <152319369+nmadden2027@users.noreply.github.com>
2024-04-11 15:10:54 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dr. Johnson! This is Niko.");
+	}
+
+}
